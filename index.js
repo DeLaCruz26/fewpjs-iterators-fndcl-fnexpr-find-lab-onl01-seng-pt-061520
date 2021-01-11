effe41f5ac0d506e6c2a03e05fb89b47a0f739fe
DeLaCruz26 <dametrik.fick@gmail.com>
2021-01-11 03:45:28 -0500

    commit tree 570374f671056adc6019f0a276908ca6271482a4

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+ let result = record.find(r => r.result === "W")
+  return result ? result.year : undefined
+}
